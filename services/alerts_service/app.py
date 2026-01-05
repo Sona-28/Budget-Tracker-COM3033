8c55252cd04d4ac54184d0fc5b1d0bcd7a7b23b1
Abhishek <abhishekkarthik24@gmail.com>
2026-01-05 18:43:50 +0000

    Dockerisation

--- services/alerts_service/app.py
+++ services/alerts_service/app.py
@@ -6,6 +6,7 @@ def create_app():
     app.register_blueprint(alerts_api)
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(port=5005, debug=True)
